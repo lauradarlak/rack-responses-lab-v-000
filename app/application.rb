9eba7c3b3444315fe306cc7e89c6a87ee6f98541
Laura Darlak <laura@polyporedesign.com>
2018-10-12 19:21:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,17 +8,6 @@ class Application
       resp.write "Good Morning!"
     else resp.write "Good Afternoon!"
     end
-
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
-
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
-    end
-
     resp.finish
   end
 
